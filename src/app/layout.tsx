a2707518376a570fc12e4938db9d83cd8d0ec63b
baotran1311 <baotran13112003@gmail.com>
2025-03-02 20:42:32 +0700

    feat/setup font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
+import localFont from "next/font/local";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import "./app.css";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
@@ -8,8 +17,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={alpino.className}>
       <body>{children}</body>
+
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
